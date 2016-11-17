c9cc120981f2337cf6b9df97072a50d02e02afd6
aedm <korteur@gmail.com>
2016-11-17 23:17:27 +0100

    Docker version doesn't use root

--- docker/onbuild.sh
+++ docker/onbuild.sh
@@ -1,30 +1,41 @@
 #!/bin/sh
 
 # Install build tools
-apt-get update
-apt-get -y install curl procps python g++ make
+apt-get -qq update
+apt-get -y install curl procps python g++ make sudo >/dev/null
+
+# Create non-root user
+useradd -m user
+sudo -u user cp -r /app /home/user/
+ls /home/user
+cd /home/user/app
 
 # Install Meteor
 curl "https://install.meteor.com/" | sh
 
 # Install NPM packages
-meteor npm install
+sudo -u user meteor npm install
 
 # Build Meteor app
-meteor --allow-superuser build --directory /meteor-app
+sudo -u user meteor build --directory /home/user/meteor-app
 
 # Install NPM packages in the bundle
-cd /meteor-app/bundle/programs/server 
-meteor npm install
+cd /home/user/meteor-app/bundle/programs/server
+pwd
+sudo -u user meteor npm install
 
 # Copy Node.js executable from Meteor distribution
-cp `meteor node -e "console.log(process.argv[0]);"` ./ 
+cp `sudo -u user meteor node -e "console.log(process.argv[0]);"` ./
+rm -rf /home/user/app
 
 # Remove Meteor
 rm /usr/local/bin/meteor
 rm -rf ~/.meteor
+rm -rf /home/user/.meteor
 
 # Remove build tools and empty cache
-apt-get -y --purge autoremove curl procps python g++ make 
-apt-get -y clean 
+apt-get -y --purge autoremove curl procps python g++ make
+apt-get -y clean
 rm -rf /var/lib/apt/lists/*
+
+ls -la /home/user
