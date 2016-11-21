9af3d8b4aff913c8bf813d365cfebba1d072b959
aedm <korteur@gmail.com>
2016-11-21 01:13:46 +0100

    Fixes user rights for app directory.

--- docker/onbuild.sh
+++ docker/onbuild.sh
@@ -1,35 +1,44 @@
 #!/bin/sh
 
 # Install build tools
+echo [minimeteor] Installing build tools
 apt-get -qq update
 apt-get -y install curl procps python g++ make sudo >/dev/null
 
 # Create non-root user
 useradd -m user
 sudo -u user cp -r /app /home/user/
+rm -rf /home/user/app/.meteor/local
+rm -rf /home/user/app/node_modules
 cd /home/user/app
 
 # Install Meteor
-curl "https://install.meteor.com/" | sh
+echo [minimeteor] Installing Meteor
+sudo -u user curl "https://install.meteor.com/" | sh
 
 # Install NPM packages
+echo [minimeteor] Installing NPM dependencies
 sudo -u user meteor npm install
 
 # Build Meteor app
+echo [minimeteor] Building Meteor app
 sudo -u user meteor build --directory /home/user/meteor-app
 
 # Install NPM packages in the bundle
+echo [minimeteor] Install bundle NPM dependencies
 cd /home/user/meteor-app/bundle/programs/server
 sudo -u user meteor npm install
 
 # Copy Node.js executable from Meteor distribution
 cp `sudo -u user meteor node -e "console.log(process.argv[0]);"` ./
-rm -rf /home/user/app
 
 # Remove Meteor
+echo [minimeteor] Cleaning up
+rm -rf /home/user/app
 rm /usr/local/bin/meteor
 rm -rf ~/.meteor
 rm -rf /home/user/.meteor
+rm -rf /tmp
 
 # Remove build tools and empty cache
 apt-get -y --purge autoremove curl procps python g++ make
